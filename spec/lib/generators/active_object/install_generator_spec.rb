d76e766a2e994ca7564b28772e5bb1581cb858a5
Juan Gomez <j.gomez@drexed.com>
2016-06-01 16:32:16 -0400

    updates

--- spec/lib/generators/active_object/install_generator_spec.rb
+++ spec/lib/generators/active_object/install_generator_spec.rb
@@ -1,15 +1,15 @@
 require "spec_helper"
 
 describe ActiveObject::InstallGenerator, type: :generator do
-  destination(File.expand_path('../../tmp', __FILE__))
+  destination(File.expand_path("../../tmp", __FILE__))
 
   before(:all) do
     prepare_destination
     run_generator
   end
 
-  it 'to be true' do
-    sample_path = 'spec/lib/generators/tmp/config/initializers/active_object.rb'
+  it "to be true" do
+    sample_path = "spec/lib/generators/tmp/config/initializers/active_object.rb"
 
     expect_file = File.read('spec/support/generators/sample.rb')
     sample_file = File.read(sample_path)
